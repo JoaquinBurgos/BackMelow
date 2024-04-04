3d7208e5352c032065745169d952e8308ae76632
JoaquinBurgos <48300178+JoaquinBurgos@users.noreply.github.com>
2024-04-04 16:58:15 +0200

    Delete 20240401093502_change_conditions_to_belong_to_campaign_directly2.rb
    delete a migration that is bugged

--- campaign_service/db/migrate/20240401093502_change_conditions_to_belong_to_campaign_directly2.rb
+++
@@ -1,9 +0,0 @@
-class ChangeConditionsToBelongToCampaignDirectly2 < ActiveRecord::Migration[6.0]
-  def change
-    # Solo elimina la tabla si estás seguro de que no se necesita.
-    drop_table :campaign_conditions
-
-    # Añadir campaign_id a la tabla conditions.
-    add_reference :conditions, :campaign, null: false, index: true, foreign_key: true
-  end
-end
